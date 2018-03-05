08e45351c5320948ab156bb58d9a7a7dfd8c6b14
sebastiannieto27 <sebastian.nietop@gmail.com>
2018-03-04 20:32:44 -0500

    Agregar archivos de proyecto

---
+++ PruebaDomicilios/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  customTableViewCell
+//
+//  Created by Yash Patel on 01/11/17.
+//  Copyright © 2017 Yash Patel. All rights reserved.
+//
+
+import UIKit
+
+ let array_Id_elements = ["1001", "1002", "1003", "1004"]
+ var myRoute = 0
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let array_Id_elements = ["1001", "1002", "1003", "1004"]
+    let array_NameRoute_elements = ["Ruta 01", "Ruta 02", "Ruta 03", "Ruta 04"]
+    let array_NameSchool_elements = ["Colegio Colombia", "Colegio Cundinamarca", "Colegio Huila", "Colegio Nariño"]
+    let array_Image_elements=["Bus-128","schooolbus-128","Citycons_bus-128","aiga_bus_on_grn_circle-128"]
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array_Id_elements.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! CustomTableViewCell
+        
+        cell.cellView.layer.cornerRadius = cell.cellView.frame.height / 2
+        
+        cell.lb_IdRoute.text = array_Id_elements[indexPath.row]
+        cell.ld_NameRoute.text = array_NameRoute_elements[indexPath.row]
+        cell.lb_NameSchool.text = array_NameSchool_elements[indexPath.row]
+        cell.busImage.image = UIImage(named: array_Image_elements[indexPath.row])
+        cell.busImage.layer.cornerRadius = cell.busImage.frame.height / 2
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        myRoute = indexPath.row
+        performSegue(withIdentifier: "ShowMap", sender: self)
+        
+    }
+    
+   
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
