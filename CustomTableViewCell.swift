10b4ee6fef49f9ae5850e8c5cceccbb1fff04222
sebastiannieto27 <sebastian.nietop@gmail.com>
2018-03-04 21:27:45 -0500

    Update CustomTableViewCell.swift

--- CustomTableViewCell.swift
+++ CustomTableViewCell.swift
@@ -6,21 +6,29 @@
 //  Copyright © 2017 Yash Patel. All rights reserved.
 //
 
+
+
+/*IBOutlets se utilizan para hacer referencia a algo que está en la Interfaz Grafica. Digamos que hay un botón en el
+*Interfaz Grafica que debe tener acceso en el código, puede controlarlo y arrastrarlo al archivo de clase viewcontroller asociado.
+*
+*
+*/
+
 import UIKit
 
 class CustomTableViewCell: UITableViewCell {
-
+ 
+    // Aqui se hace referencia  la imagen de cada ruta
     @IBOutlet weak var busImage: UIImageView!
+    // Aqui  se hace referencia a la celda que contiene la informacion
     @IBOutlet weak var cellView: UIView!
-    
+    // Aqui el label donde se muestra el id de la ruta
     @IBOutlet weak var lb_IdRoute: UILabel!
-    
+    // Aqui el label donde se muestra el nombre de la ruta
     @IBOutlet weak var ld_NameRoute: UILabel!
-    
+    // Aqui el label donde se muestra el nombre del colegio
     @IBOutlet weak var lb_NameSchool: UILabel!
-    @IBAction func goToMap(sender: AnyObject) {
-        
-    }
+   
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
